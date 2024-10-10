0ad87d8c0d492e35fe3cec02c6d118f31e54f4cb
“Waylon06” <205218290@qq.com>
2024-10-11 02:49:00 +0800

    1.1.2

--- server-alumni-website/src/main/java/com/school/utils/UploadUtil.java
+++ server-alumni-website/src/main/java/com/school/utils/UploadUtil.java
@@ -9,18 +9,18 @@ import java.io.IOException;
 import java.util.UUID;
 
 public class UploadUtil {
-    public static final String ALI_DOMAIN = "https://waylon-personal-bucket.oss-cn-chengdu.aliyuncs.com/";
+    public static final String ALI_DOMAIN = "";
     public static String uploadImg(MultipartFile imgFile) throws IOException {
         String originalFilename = imgFile.getOriginalFilename();
         String ext = "." + FilenameUtils.getExtension(originalFilename);
         String uuid = UUID.randomUUID().toString().replace("-", "");
         String fileName = uuid + ext;
 //        地域节点
-        String endpoint = "https://oss-cn-chengdu.aliyuncs.com";
-        String accessKeyId = "";
-        String accessKeySecret = "";
+        String endpoint = "";
+        // String accessKeyId = "";
+        // String accessKeySecret = "";
         OSS build = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        build.putObject("waylon-personal-bucket", fileName, imgFile.getInputStream());
+        build.putObject("", fileName, imgFile.getInputStream());
         build.shutdown();
         return ALI_DOMAIN + fileName;
     }
